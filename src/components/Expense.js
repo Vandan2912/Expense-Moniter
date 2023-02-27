6afb84ac5c9e4062cf0fc47ce02c6953d7e7d899
Vandan Patel <vandanpatel29122001@gmail.com>
2023-02-27 16:06:49 +0530

    tab added succesfully

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 export const Expense = (props) => {
   const expensesData = [
@@ -40,98 +42,107 @@ export const Expense = (props) => {
     setDates(new Date(RData.date).toISOString().substring(0, 10));
   };
 
-  console.log(RData.date, Dates);
-
   return (
     <div className="App">
-      <div className="new-expense">
-        <form>
-          <div className="new-expense__controls">
-            <div className="new-expense__control">
-              <label>Title</label>
-              <input
-                type="text"
-                value={Title}
-                onChange={(e) => {
-                  setTitle(e.target.value);
-                }}
-              />
-            </div>
-            <div className="new-expense__control">
-              <label>Amount</label>
-              <input
-                type="number"
-                min="0.01"
-                step="0.01"
-                value={Amount}
-                onChange={(e) => {
-                  setAmount(e.target.value);
-                }}
-              />
-            </div>
-            <div className="new-expense__control">
-              <label>Date</label>
-              <input
-                type="date"
-                min="2019-01-01"
-                max="2023-12-31"
-                value={Dates}
-                onChange={(e) => {
-                  setDates(e.target.value);
-                }}
-              />
-            </div>
-          </div>
-          <div className="new-expense__actions">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                if (UpdateForm) {
-                  console.log(RData);
-                  const updateData = {
-                    id: RData.id,
-                    title: Title,
-                    amount: Amount,
-                    date: Dates,
-                  };
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
 
-                  let newData = Data.map((item) => {
-                    if (parseInt(item.id) === parseInt(updateData.id)) {
-                      item.id = updateData.id;
-                      item.title = Title;
-                      item.amount = Amount;
-                      item.date = new Date(Dates);
+        <TabPanel>
+          <div className="new-expense">
+            <form>
+              <div className="new-expense__controls">
+                <div className="new-expense__control">
+                  <label>Title</label>
+                  <input
+                    type="text"
+                    value={Title}
+                    onChange={(e) => {
+                      setTitle(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="new-expense__control">
+                  <label>Amount</label>
+                  <input
+                    type="number"
+                    min="0.01"
+                    step="0.01"
+                    value={Amount}
+                    onChange={(e) => {
+                      setAmount(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="new-expense__control">
+                  <label>Date</label>
+                  <input
+                    type="date"
+                    min="2019-01-01"
+                    max="2023-12-31"
+                    value={Dates}
+                    onChange={(e) => {
+                      setDates(e.target.value);
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="new-expense__actions">
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    if (UpdateForm) {
+                      console.log(RData);
+                      const updateData = {
+                        id: RData.id,
+                        title: Title,
+                        amount: Amount,
+                        date: Dates,
+                      };
+
+                      let newData = Data.map((item) => {
+                        if (parseInt(item.id) === parseInt(updateData.id)) {
+                          item.id = updateData.id;
+                          item.title = Title;
+                          item.amount = Amount;
+                          item.date = new Date(Dates);
+                        }
+                        return item;
+                      });
+                      console.log(newData);
+                      setData(newData);
+                      setUpdateForm(false);
+                    } else {
+                      setId(Id + 1);
+                      e.preventDefault();
+                      const newData = {
+                        id: Id,
+                        title: Title,
+                        amount: Amount,
+                        date: Dates,
+                      };
+                      console.log(newData);
+                      setData((Data) => [...Data, newData]);
                     }
-                    return item;
-                  });
-                  console.log(newData);
-                  setData(newData);
-                  setUpdateForm(false);
-                } else {
-                  setId(Id + 1);
-                  e.preventDefault();
-                  const newData = {
-                    id: Id,
-                    title: Title,
-                    amount: Amount,
-                    date: Dates,
-                  };
-                  console.log(newData);
-                  setData((Data) => [...Data, newData]);
-                }
-                setTitle("");
-                setAmount("");
-                setDates("");
-              }}
-            >
-              {UpdateForm ? "Update" : "Add Expense"}
-            </button>
+                    setTitle("");
+                    setAmount("");
+                    setDates("");
+                  }}
+                >
+                  {UpdateForm ? "Update" : "Add Expense"}
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-      {Data.map((item) => (
-        <ExpenseItem data={item} onUpdateData={updateData} />
-      ))}
+        </TabPanel>
+        <TabPanel>
+          {Data.map((item) => (
+            <ExpenseItem data={item} onUpdateData={updateData} />
+          ))}
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
